5d5c21a63568cc0dc0c86a0343a7d84085f0a149
Fowl3r <tcd.fowler@gmail.com>
2023-01-10 15:29:30 +0000

    data seeded into users table

--- App.js
+++ App.js
@@ -14,5 +14,5 @@ const app = express();
  * set app to listen to port 3001
  */
 app.listen("3001", function () {
-  console.log("Server is listening to port 3001.");
+	console.log("Server is listening to port 3001.");
 });
