02f123dbf1528f811db2368e3c7795cb2fa3bb62
tariba <tariqxa@gmail.com>
2023-01-11 14:09:01 +0000

    Get data by ID test completed

--- routes/index.test.js
+++ routes/index.test.js
@@ -1,23 +1,44 @@
 import {test, expect} from '@jest/globals';
-import supertest from 'supertest';
+import request from 'supertest';
 import app from '../App';
 import {pool} from "../db/index";
 
-afterAll(async () => {
-	await pool.end();
-});
 
+    describe('/:id', ()=>{  
+        test(`Get user favourites by ID`, async () => {
+            const response = await request(app)
+            .get("/userfavourites/1")
+            .set("Application", "application/json")
+            expect(response.status).toEqual(200)
+            console.log(response.body)
+            expect(response.body).toEqual({
+                success: true,
+                payload: expect.any(Array)  
+            })
+            const data = response.body.payload
+            console.log(data)
+            for(let i = 0; i<data.length; i++){
+                expect(data[i]).toEqual({
+                    id: expect.any(Number),
+                    title: expect.any(String),
+                    city: expect.any(String),
+                    country: expect.any(String),
+                    suburb: expect.any(String),
+                    description: expect.any(String),
+                    image: expect.any(String)
+                })
+            }
+        })
+       
 
-describe('/:id', ()=>{
-    test('Get user favourites by ID', async () => {
-        const response = await supertest(app)
-        .get("/userfavourites/1")
-        .set("Application", "application/json")
-        expect(response.status).toBe(200)
-        await pool.end();
+       
     })
     
+    afterAll(() => {
+        pool.end();
+    });
     
-    
-})
+
+
+
 
