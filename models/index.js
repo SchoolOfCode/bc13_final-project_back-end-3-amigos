4b2851af55c016fa8ccaf0f1915d6b86fa134cff
jo <jyotshnacs@gmail.com>
2023-01-23 10:04:42 +0000

    models and routes

--- models/index.js
+++ models/index.js
@@ -7,24 +7,68 @@ import { pool } from "../db/index.js";
 /**
  *  an async await fn to get all the favourites by the uid
  */
-export async function getFavouritesByUserId(uid) {
+
+// POST add new user
+export async function addNewUsers(users) {
+  const res = await pool.query(
+    `INSERT INTO users (username, email, password, uid) VALUES ($1, $2, $3, $4) RETURNING*;`,
+    [users.username, users.email, users.password, users.uid]
+  );
+  return res.rows;
+}
+
+export async function getUser(uid) {
+  const res = await pool.query(
+    `SELECT username, email, password, uid from users where uid=$1;`,
+    [uid]
+  );
+
+  return res.rows;
+}
+
+export async function deleteUser(uid) {
+  const res = await pool.query(`DELETE FROM users where uid=$1;`, [uid]);
+
+  return res.rows;
+}
+
+export async function getUsers() {
+  const res = await pool.query(
+    `SELECT username, email, password, uid from users;`
+  );
+  return res.rows;
+}
+
+export async function getFavouritesByUid(uid) {
   // use pool.query to open database connection
 
   const res = await pool.query(
     // use the inner join to select user's favourite places by uid
     // used the $1 to secure the uid
-    `SELECT user_favourites.uid, xid, title, city, country, suburb, description, image FROM user_favourites INNER JOIN users on user_favourites.uid=users.uid WHERE user_favourites.uid= $1`,
+    `SELECT uid, xid, title, city, country, suburb, description, image FROM user_favourites where uid=$1`,
     [uid]
   );
   return res.rows;
 }
 
-// POST- add new user favourite to database 
-export async function addNewFavourite(favourite) {
+export async function deleteFavouritesByUid(uid) {
+  // use pool.query to open database connection
+
+  const res = await pool.query(
+    // use the inner join to select user's favourite places by uid
+    // used the $1 to secure the uid
+    `DELETE FROM user_favourites where uid=$1`,
+    [uid]
+  );
+  return res.rows;
+}
+
+// POST- add new user favourite to database
+export async function addFavourite(uid, favourite) {
   const res = await pool.query(
     `INSERT INTO user_favourites (uid, xid, title, city, country, suburb, description, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING*;`,
     [
-      favourite.uid,
+      uid,
       favourite.xid,
       favourite.title,
       favourite.city,
@@ -37,20 +81,19 @@ export async function addNewFavourite(favourite) {
   return res.rows;
 }
 
-// POST add new user
-export async function addNewUsers(users) {
+export async function getFavouriteByUidXid(uid, xid) {
   const res = await pool.query(
-    `INSERT INTO users (username, email, password, uid) VALUES ($1, $2, $3, $4) RETURNING*;`,
-    [users.username, users.email, users.password, users.uid]
+    `SELECT  uid, xid, title, city, country, suburb, description, image FROM user_favourites WHERE uid = $1 AND xid = $2;`,
+    [uid, xid]
   );
   return res.rows;
 }
 
 //DELETE - location by xid AND uid, which comes through as an object from the front end
-export async function deleteFavouriteById(data) {
+export async function deleteFavouriteByUidXid(uid, xid) {
   const res = await pool.query(
-    `DELETE FROM user_favourites WHERE xid = $1 AND uid = $2 RETURNING*;`,
-    [data.xid, data.uid]
+    `DELETE FROM user_favourites WHERE uid = $1 AND xid = $2 RETURNING*;`,
+    [uid, xid]
   );
   return res.rows;
 }
--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,21 @@
 
 import express from "express";
 import {
-  getFavouritesByUserId,
-  addNewFavourite,
+  getFavouritesByUid,
+  deleteFavouritesByUid,
+  getFavouriteByUidXid,
+  deleteFavouriteByUidXid,
+  addFavourite,
   addNewUsers,
-  deleteFavouriteById,
+  getUser,
+  deleteUser,
+  getUsers,
 } from "../models/index.js";
 
 const router = express.Router();
 
-// GET: favourites by uid
-router.get("/:uid", async function (req, res) {
-  const response = await getFavouritesByUserId(req.params.uid);
+router.get("/", async function (req, res) {
+  const response = await getUsers();
   res.status(200).json({
     success: true,
     payload: response,
@@ -29,15 +33,56 @@ router.post("/", async function (req, res) {
   console.log(req.body);
   // changed username to uid to validate the user.
   if (req.body.uid) {
-    const response = await addNewUsers(req.body);
-    console.log(response);
-    res.status(200).json({
-      success: true,
-      payload: response,
-    });
-  } else {
-    const response = await addNewFavourite(req.body);
-    console.log(response);
+    const currentUser = await getUser(req.body.uid);
+    console.log(`current user: ${currentUser}`);
+
+    if (currentUser.length === 0) {
+      const response = await addNewUsers(req.body);
+      console.log(response);
+      res.status(201).json({
+        success: true,
+        payload: response,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        payload: currentUser,
+      });
+    }
+  }
+});
+
+router.get("/:uid", async function (req, res) {
+  console.log(req.params);
+  const response = await getUser(req.params.uid);
+  res.status(200).json({
+    success: true,
+    payload: response,
+  });
+});
+
+router.delete("/:uid", async function (req, res) {
+  const response = await deleteUser(req.params.uid);
+  res.status(200).json({
+    success: true,
+    payload: response,
+  });
+});
+// GET: favourites by uid
+router.get("/:uid/favourites", async function (req, res) {
+  const response = await getFavouritesByUid(req.params.uid);
+  res.status(200).json({
+    success: true,
+    payload: response,
+  });
+});
+
+router.post("/:uid/favourites", async function (req, res) {
+  // changed username to uid to validate the user.
+  console.log(req.params.uid);
+  console.log(req.body);
+  if (req.params.uid) {
+    const response = await addFavourite(req.params.uid, req.body);
     res.status(200).json({
       success: true,
       payload: response,
@@ -45,9 +90,31 @@ router.post("/", async function (req, res) {
   }
 });
 
+router.delete("/:uid/favourites", async function (req, res) {
+  // changed username to uid to validate the user.
+  if (req.params.uid) {
+    const response = await deleteFavouritesByUid(req.params.uid);
+    res.status(200).json({
+      success: true,
+      payload: response,
+    });
+  }
+});
+
+router.get("/:uid/favourites/:xid", async function (req, res) {
+  const response = await getFavouriteByUidXid(req.params.uid, req.params.xid);
+  res.status(200).json({
+    success: true,
+    payload: response,
+  });
+});
+
 // DELETE: by uid and xid, passed through as an object in the body from the front end
-router.delete("/", async function (req, res) {
-  const response = await deleteFavouriteById(req.body);
+router.delete("/:uid/favourites/:xid", async function (req, res) {
+  const response = await deleteFavouriteByUidXid(
+    req.params.uid,
+    req.params.xid
+  );
   res.status(200).json({
     success: true,
     payload: response,
